1edc4b3d3406447728ce1de7f1aa83674b153eb1
Mark Needham <m.h.needham@gmail.com>
2018-11-27 11:24:05 +0000

    fixing images

--- render_guide.sh
+++ render_guide.sh
@@ -13,14 +13,14 @@ function render {
 # -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
 if [ "$1" == "publish" ]; then
   URL=guides.neo4j.com/applied_graph_algorithms
-  render http://$URL -a env-training
+  render http://$URL -a env-training -a img=https://$URL/images
   s3cmd put --recursive -P *.html img s3://${URL}/
   s3cmd put -P index.html s3://${URL}
   echo "Publication Done"
 else
   URL=localhost:8001
 # copy the csv files to $NEO4J_HOME/import
-  render http://$URL -a env-training
+  render http://$URL -a env-training -a img=http://$URL/images
   echo "Starting Websever at $URL Ctrl-c to stop"
   python $GUIDES/http-server.py
   # python -m SimpleHTTPServer
