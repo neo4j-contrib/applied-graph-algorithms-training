0e7158742045b486422020aecf9b3435c89d4b95
Mark Needham <m.h.needham@gmail.com>
2018-12-10 13:51:11 +0000

    flask app to explore photos

---
+++ photos.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template
+import neo4j
+
+app = Flask(__name__)
+driver = neo4j.Driver("bolt://localhost", auth=("neo4j", "neo"))
+
+
+@app.route("/")
+def photos():
+    with driver.session() as session:
+        rows = session.run("""
+        MATCH (p1:Photo)-[r:SIMILAR]->(p2:Photo)
+        RETURN p1.id AS p1, r.score AS score, p2.id AS p2,
+               [(p1)-[:HAS_LABEL]->(label) | label.description] AS p1Labels,
+               [(p2)-[:HAS_LABEL]->(label) | label.description] AS p2Labels
+        ORDER BY score
+        LIMIT 100
+        """)
+
+        photos = [row for row in rows]
+
+    return render_template('photos.html', photos=photos)
+
+@app.route("/clusters/<cluster_id>")
+def clusters(cluster_id):
+    with driver.session() as session:
+        rows = session.run("""
+        MATCH (p:Photo {partition: {clusterId}})
+        RETURN p.id AS p1
+        """, {"clusterId": int(cluster_id)})
+
+        photos = [row for row in rows]
+
+    return render_template('clusters.html', photos=photos)
