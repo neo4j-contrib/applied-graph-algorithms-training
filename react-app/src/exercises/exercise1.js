257bd1fec1c3c83e3304b2b7f95b5393b44b6c3f
Mark Needham <m.h.needham@gmail.com>
2018-12-11 09:35:44 +0000

    more exploration

--- react-app/src/exercises/exercise1.js
+++ react-app/src/exercises/exercise1.js
@@ -1,10 +1,10 @@
 /*
  TODO: instructions / context for
  */
-export const FETCH_CATEGORIES_QUERY = 
+export const FETCH_CATEGORIES_QUERY =
 `
 MATCH (c:Category)
-WHERE NOT EXISTS ((c)-[:NARROWER_THAN]->())
+WHERE (NOT exists ((c)-[:NARROWER_THAN]->())) AND exists((c)<-[:NARROWER_THAN]-())
 WITH c LIMIT 50
-RETURN COLLECT(c.name) AS categories 
+RETURN COLLECT(c.name) AS categories
 `;
