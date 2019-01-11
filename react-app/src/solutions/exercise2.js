e26132eb9789c6e9fa74a2fc2dee1c747b1dfcbf
johnymontana <lyonwj@gmail.com>
2019-01-11 13:00:03 -0700

    update solution query

--- react-app/src/solutions/exercise2.js
+++ react-app/src/solutions/exercise2.js
@@ -8,7 +8,6 @@ OPTIONAL MATCH (b)-[:HAS_PHOTO]->(p:Photo)
 WITH b, COLLECT(p)[0] AS p
 MATCH (me:User {id: $userId})
 OPTIONAL MATCH (me)-[similarity:SIMILAR]-(other)-[:WROTE]-(:Review)-[:REVIEWS]->(b)
-WITH *, avg(coalesce(similarity.score,0.0)) AS num
-WITH * ORDER BY num DESC LIMIT 100
-RETURN COLLECT(b {.*, photo: p.id}) AS businesses
+WITH DISTINCT b,p, avg(coalesce(similarity.score,0)) AS num ORDER BY num DESC LIMIT 100
+RETURN COLLECT(b {.*, photo: p.id, score: num}) AS businesses
 `;
