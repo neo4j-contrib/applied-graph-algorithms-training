fc0a9cbce82bf898fc6d3418fdbab2e7762340f5
johnymontana <lyonwj@gmail.com>
2019-01-11 06:09:02 -0700

    update exercise 1 queries and solution

--- react-app/src/solutions/exercise1.js
+++ react-app/src/solutions/exercise1.js
@@ -1,9 +1,8 @@
-/*
- TODO: instructions / context for
- */
 export const FETCH_CATEGORIES_QUERY =
+
 `
 MATCH (c:Category)
-WITH c ORDER BY c.name LIMIT 500
+WHERE NOT EXISTS ((c)-[:NARROWER_THAN]->())
+WITH c ORDER BY c.name LIMIT 200
 RETURN COLLECT(c.name) AS categories
 `;
