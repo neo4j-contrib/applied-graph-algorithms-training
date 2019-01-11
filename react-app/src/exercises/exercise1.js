2a4437bdbcdbf2d9f25618c80d1ab5479189975b
johnymontana <lyonwj@gmail.com>
2019-01-10 17:55:59 -0700

    app updates

--- react-app/src/exercises/exercise1.js
+++ react-app/src/exercises/exercise1.js
@@ -8,9 +8,3 @@ WHERE NOT EXISTS ((c)-[:NARROWER_THAN]->())
 WITH c ORDER BY c.name LIMIT 200
 RETURN COLLECT(c.name) AS categories
 `;
-
-// `
-// MATCH (c:Category)
-// WITH c ORDER BY c.name LIMIT 500
-// RETURN COLLECT(c.name) AS categories
-// `;
--- react-app/src/exercises/exercise4.js
+++ react-app/src/exercises/exercise4.js
@@ -2,6 +2,14 @@
 export const FETCH_RECOMMENDED_PHOTOS_QUERY =
 `
 MATCH (b:Business)-[:HAS_PHOTO]->(p:Photo)
-WITH b, p SKIP toInteger(rand() * 30000)-100 LIMIT 100
-RETURN p{photoId: p.id, businessId: b.id} AS photos
+WITH b, p SKIP toInteger(rand() * 30000)-1000 LIMIT 10000
+WITH COLLECT(p{photoId: p.id, businessId: b.id}) AS photos
+RETURN apoc.coll.shuffle(photos)[..199] AS photos
+`;
+
+export const FETCH_PHOTO_RECOMMENDATIONS = 
 `
+UNWIND $selectedPhotos AS photoId
+MATCH (p:Photo {id: photoId})<-[:HAS_PHOTO]-(b:Business)
+RETURN COLLECT(b {.*, photo: p.id}) AS recommendations
+`;
