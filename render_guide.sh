0be207a42a3f6a5fbcc703e2a434fe0f3b98aa75
johnymontana <lyonwj@gmail.com>
2019-03-16 10:37:05 -0600

    add overview guide and update guide urls

--- render_guide.sh
+++ render_guide.sh
@@ -5,17 +5,17 @@ GUIDES=../neo4j-guides
 # git clone http://github.com/jexp/neo4j-guides $GUIDES
 
 function render {
-  # $GUIDES/run.sh index.adoc index.html +1 "$@"
+  $GUIDES/run.sh index.adoc index.html +1 "$@"
   $GUIDES/run.sh 01_cypher_refresher.adoc 01_cypher_refresher.html +1 "$@"
   $GUIDES/run.sh 02_category_hierarchy.adoc 02_category_hierarchy.html +1 "$@"
-  $GUIDES/run.sh 03_relevant_reviews.adoc 03_relevant_reviews.html +1 "$@"
-  $GUIDES/run.sh 04_search_results.adoc 04_search_results.html +1 "$@"
+  $GUIDES/run.sh 03_relevant_reviews.adoc 04_relevant_reviews.html +1 "$@"
+  $GUIDES/run.sh 04_search_results.adoc 03_search_results.html +1 "$@"
   $GUIDES/run.sh 05_photo_recommendations.adoc 05_photo_recommendations.html +1 "$@"
 }
 
 # -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
 if [ "$1" == "publish" ]; then
-  URL=guides.neo4j.com/applied_graph_algorithms
+  URL=guides.neo4j.com/sandbox/applied_graph_algorithms
   render http://$URL -a env-training -a img=https://$URL/images
   s3cmd put --recursive -P *.html img s3://${URL}/
   s3cmd put -P index.html s3://${URL}
